38622f421ca4a1d5e54833f50284448a94b442ef
Daniel-KI <dankorshov@gmail.com>
2020-12-20 15:40:44 +0300

    refactor: white-space

--- Calculator.js
+++ Calculator.js
@@ -15,6 +15,7 @@ method updateDisplay() -> Display values on page
 */
 
 class Calculator {
+    
     constructor(previousOperandTextElement, currentOperandTextElement) {
         this.previousOperandTextElement = previousOperandTextElement;
         this.currentOperandTextElement = currentOperandTextElement;
